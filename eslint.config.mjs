608c15bbd972e04a6b028b422baf27f347007990
Tarun Monga <tarunmonga2208@gmail.com>
2025-05-07 13:32:46 +0530

    third

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,21 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+// Emulate __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Create a compat instance with the base directory
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// Define ESLint config using compat.config()
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+  }),
 ];
 
 export default eslintConfig;
+
